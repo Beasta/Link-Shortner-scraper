7103847b407e49fdda8f7e6f49bf4f2bc966a4f8
Barry Blaha <barryblaha@gmail.com>
2015-12-10 09:45:25 -0800

    Code cleanup

--- htmlScrape.js
+++ htmlScrape.js
@@ -1,5 +1,5 @@
 //Scraper will search a website for valid links that have been shortened. 
-//The 
+
 var request = require('request'),
   zlib = require('zlib'),
   fs = require('fs'),
@@ -17,12 +17,6 @@ var headers = {
   "DNT":1,
   "Host":important.site,
   'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.13+(KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
-
-  //"accept-charset" : "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-  //"accept-language" : "en-US,en;q=0.8",
-  //"accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-  //"user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
-  //"accept-encoding" : "gzip,deflate",
 };
 
 var options = {
@@ -34,35 +28,41 @@ var options = {
 var counter = 599;
 
 var requestWithEncoding = function(options, callback) {
+
   console.log('counter',counter);
   console.log('typeof callback',typeof callback);
-  extension = ( counter++ ).toString(16);
+
+  //extension to be tested is 3 hexadecimal digits
+  extension = ( counter++ ).toString(16); 
   if (extension.length === 1){
     extension = "00"+extension;
   }else if(extension.length === 2){
     extension = "0" + extension;
   }
+
   console.log('extension:',extension);
+
   options.url = "http://" + important.site + "/" + extension; 
-  if(counter ===1100){
+
+  //for 3 hexadecimal digits counter = 16*16*16 = 4096
+  if(counter === 1100){
     console.log('counter has reached '+counter);
     clearInterval(requestAll);
   }
+
   var req = request.get(options);
 
   req.on('response', function(res) {
-    // console.log('res.status:',res.statusCode);
-    // console.log("headers?",res.headers);
-
     var chunks = [];
+
     res.on('data', function(chunk) {
       chunks.push(chunk);
     });
 
     res.on('end', function() {
-      // console.log("headers?",res.headers);
       var buffer = Buffer.concat(chunks);
       var encoding = res.headers['content-encoding'];
+
       if (encoding == 'gzip') {
         zlib.gunzip(buffer, function(err, decoded) {
           callback(err, decoded && decoded.toString());
@@ -81,43 +81,23 @@ var requestWithEncoding = function(options, callback) {
   req.on('error', function(err) {
     callback(err);
   });
-};
 
-// requestWithEncoding(options, function(err, data) {
-//   if (err) console.log('error',err);
-//   else{
-//     fs.writeFile("./pages/"+extension+".html", data, function(err) {
-//       // console.log('response.id',JSON.parse(response).id);
-//     // fs.writeFile("./pages/"+Math.floor(Math.random()*10000)/*process.argv[3]*/, response, function(err) {
-//         if(err) {
-//             return console.log('error saving file',err);
-//         }
-//         // console.log("The everything file was saved!");
-//        // clearInterval(requestAll);
-//     });
-//   }
-// });
+};
 
 var theCall = function(){
       requestWithEncoding(
         {
           method: 'GET',
-          // url: "http://"+ important.site + "/" + (counter++).toString(16),
           headers: headers
         },
         function(err, data) {
           if (err) console.log('error',err);
           else{
-            if( !(/Page Not Found/.test(data) ) ) {
-
-              fs.writeFile("./pages/"+extension+".html", data, function(err) {
-              // console.log('response.id',JSON.parse(response).id);
-              // fs.writeFile("./pages/"+Math.floor(Math.random()*10000)/*process.argv[3]*/, response, function(err) {
+            if( !(/Page Not Found/.test(data) ) ) { //regex checks the page html to see if anything was found
+              fs.writeFile("./pages/"+extension+".html", data, function(err) { //if something was found, save the file in ./pages folder
                 if(err) {
                   return console.log('error saving file',err);
                 }
-              // console.log("The everything file was saved!");
-              // clearInterval(requestAll);
               });
             }
         }
@@ -125,7 +105,6 @@ var theCall = function(){
 };
 
 var requestAll = setInterval(
-  // var newExtension = counter.toString(16);
-    theCall,
+  theCall,
   theDelay*1000
 );
